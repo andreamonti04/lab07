9336cb9bc44b37a6ab841e8d26a96eeb4f74e7df
emme004 <andrea.monti24@studio.unibo.it>
2024-11-05 16:44:48 +0100

    Ex 71 corrected by tutor. All it's ok.

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,7 +1,5 @@
 package it.unibo.inner.impl;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -11,23 +9,37 @@ import it.unibo.inner.api.Predicate;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
     private List<T> elements;
+    private Predicate<T> filter;
 
     
     public IterableWithPolicyImpl(T[] alist){  //accetto lista di qualsiasi tipo, ma che sia dello stesso tipo di Iterable
-        this.elements = List.of(alist);
+        //this. campi e metodi
+        //this() mi riferisco ai costruttori
+        this(alist, new Predicate<T>(){           //utilizza il secondo costruttore, predicato ha un solo metodo
+            @Override                               //classe anonima e dare comportamento specifico della classe anonima
+            public boolean test(T elem) {
+                return true;  
+            }
+        }); 
     }
 
+    public IterableWithPolicyImpl(T[] alist, Predicate<T> filter){
+        this.elements = List.of(alist);
+        this.filter = filter;
+    }
 
     public class IteratorImpl implements Iterator<T>{
         private int index = 0;
 
-
         @Override
         public boolean hasNext() {
-            if(index < elements.size()){
-                return true;
+            while(index < elements.size()){
+                T elem = elements.get(index);
+                if(filter.test(elem)){
+                    return true;
+                }
+                index++;
             }
-            index++;
             return false;
         }
 
@@ -42,13 +54,12 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
 
     @Override
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return new IteratorImpl();    
     }
 
     @Override
-    public void setIterationPolicy(Predicate filter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+    public void setIterationPolicy(Predicate<T> filter) {
+        this.filter = filter;   
     }
 }
