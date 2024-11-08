8dd0a3026408d089e20496b073b63ebb42a7995d
emme004 <andrea.monti24@studio.unibo.it>
2024-11-08 15:47:29 +0100

    It's ok

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -23,12 +23,13 @@ public final class MonthSorterNested implements MonthSorter {
     }
 
     private enum Month {
+        
         JANUARY(31), FEBRUARY(28), MARCH(31), APRIL(30),
         MAY(31), JUNE(30), JULY(31), AUGUST(31),
         SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
 
+        
         private final int days;
-
         Month(final int days) {
             this.days = days;
         }
